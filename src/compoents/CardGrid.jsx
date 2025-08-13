c8c6af1760d4649f6f5db3109903cae19542b742
Jithesh <47631880+jithesh042@users.noreply.github.com>
2025-08-13 15:13:21 +0530

    Update CardGrid.jsx

--- src/compoents/CardGrid.jsx
+++ src/compoents/CardGrid.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './CardGrid.css';
-
 
 export default function CardGrid({ item }) {
   const navigate = useNavigate();
   const handleClick = () => navigate(`/details/${item._id}`);
 
+  const imageUrl = item.coverImage?.startsWith('http')
+    ? item.coverImage
+    : `${import.meta.env.VITE_URL}/${item.coverImage}`;
+
   return (
     <div className="card" onClick={handleClick}>
-      <img src={item.coverImage} alt={item.title} className="card-img" />
+      <img src={imageUrl} alt={item.title} className="card-img" />
       <h3 className="card-title">{item.title}</h3>
       <p className="card-author">By {item.author}</p>
     </div>
   );
 };
-
